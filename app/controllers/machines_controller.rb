80b96ed41b1c5c6249a67ec376a58d681b5370e6
bulatkyr <bulatkyr@fit.cvut.cz>
2019-12-25 12:30:47 +0100

    KBU: added Machine controller methods

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,2 +1,43 @@
 class MachinesController < ApplicationController
+  before_action :set_machine, only: %i[show update destroy]
+
+  # GET /machines
+  def index
+    @machines = Machine.all
+    json_response(@machines)
+  end
+
+  # POST /machines
+  def create
+    @machine = Machine.create!(machine_params)
+    json_response(@machine, :created)
+  end
+
+  # GET /machines/:id
+  def show
+    json_response(@machine)
+  end
+
+  # PUT /machines/:id
+  def update
+    @machine.update(machine_params)
+    head :no_content
+  end
+
+  # DELETE /machines/:id
+  def destroy
+    @machine.destroy
+    head :no_content
+  end
+
+  private
+
+  def machine_params
+    # whitelist params
+    params.permit(:title, :created_by)
+  end
+
+  def set_machine
+    @machine = Machine.find(params[:id])
+  end
 end
