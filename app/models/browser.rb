7524ff2d25e29a7b997a4d21cc1dd42769b7f9fb
bulatkyr <bulatkyr@fit.cvut.cz>
2019-12-25 12:19:28 +0100

    KBU: added validations to models

--- app/models/browser.rb
+++ app/models/browser.rb
@@ -1,3 +1,4 @@
 class Browser < ApplicationRecord
   belongs_to :machine
+  validates_presence_of :name, :nickname
 end
--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,3 +1,4 @@
 class Machine < ApplicationRecord
-  has_many :browsers
+  has_many :browsers, dependent: :destroy
+  validates_presence_of :name, :nickname
 end
