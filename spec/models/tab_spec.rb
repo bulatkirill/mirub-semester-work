1f169aed53f3a28bcd12779d974d19d8096593f6
bulatkyr <bulatkyr@fit.cvut.cz>
2020-01-22 22:02:21 +0100

    KBU: test for Tab model

--- spec/models/tab_spec.rb
+++ spec/models/tab_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Tab, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure Tab model has a m:1 relationship with the Session model
+  it do
+    should belong_to(:session)
+  end
+  # Validation tests
+  # ensure column url is present before saving
+  it do
+    should validate_presence_of(:url)
+  end
+  # Validation tests
+  # ensure column domain is present before saving
+  it do
+    should validate_presence_of(:domain)
+  end
+
 end
