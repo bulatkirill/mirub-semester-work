df1abaef170b94e1d9b4b934cf926132741084e5
bulatkyr <bulatkyr@fit.cvut.cz>
2020-01-26 18:33:37 +0100

    KBU: added authorization of the API requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  before_action :authenticate_request
+  attr_reader :current_user
+
+  private
+
+  def authenticate_request
+    @current_user = AuthorizeRequest.call(request.headers).result
+    json_response({ error: 'Not Authorized' }, :unauthorized) unless @current_user
+  end
 end
